a13bab777ebb735b7793fc29f41ff31a83b81b15
Jeroen Marco Frank Stolk <JMST@nuuday.dk>
2021-07-15 23:13:17 +0200

    .

--- app.js
+++ app.js
@@ -1,39 +1,77 @@
 "use strict";
 
-const switcher = document.querySelector(".btn");
+const listItems = [
+  {
+    id: 1,
+    headline: "article 1",
+    text: "text article 1",
+    imgUrl: "https://via.placeholder.com/150",
+  },
+  {
+    id: 2,
+    headline: "article 2",
+    text: "text article 2",
+    imgUrl: "https://via.placeholder.com/150",
+  },
+  {
+    id: 3,
+    headline: "article 3",
+    text: "text article 3",
+    imgUrl: "https://via.placeholder.com/150",
+  },
+];
 
-switcher.addEventListener("click", function () {
-  document.body.classList.toggle("dark-theme");
-
-  var className = document.body.className;
-  if (className == "light-theme") {
-    this.textContent = "Dark";
-  } else {
-    this.textContent = "Light";
-  }
-  console.log("current class name: " + className);
+const modalClose = document.getElementById("modalClose");
+modalClose.addEventListener("click", function () {
+  document.getElementById("selected-article").classList.toggle("show");
 });
 
-const listItems = document.querySelectorAll("ul li");
-//console.log("listItems: " + listItems[1].innerHTML);
+function modalShow(elementID) {
+  const articleSelected = document.getElementById("modalContent");
+  const artId = elementID - 1;
+  const articleClone = document.querySelector("#articles").childNodes[artId];
+  console.log(articleClone);
+  articleSelected.appendChild(articleClone.cloneNode(true));
+  document.getElementById("selected-article").classList.toggle("show");
+}
 
-/* listItems.forEach((element) => {
-  console.log(element);
-  element.classList.add("test");
-}); */
+function addElement(el) {
+  const newArticle = document.createElement("div");
 
-const switcherListStyle = document.querySelector(".btnList");
+  const newArticleHeader = document.createElement("h2");
+  const newArticleHeaderText = document.createTextNode(el.text);
+  newArticleHeader.appendChild(newArticleHeaderText);
 
-switcherListStyle.addEventListener("click", function () {
-  listItems.forEach((element) => {
-    element.classList.toggle("test");
-    element.innerHTML = "<b>test</b>";
+  const newArticleBody = document.createElement("p");
+  const newArticleBodyText = document.createTextNode(el.text);
+  newArticleBody.appendChild(newArticleBodyText);
+
+  const newArticleImg = document.createElement("img");
+  newArticleImg.className = "image";
+  newArticleImg.src = el.imgUrl;
+
+  const newArticleButton = document.createElement("button");
+  const newArticleButtonText = document.createTextNode(el.text);
+  newArticleButton.appendChild(newArticleButtonText);
+
+  newArticleButton.addEventListener("click", function () {
+    document.getElementById("modalContent").textContent = null;
+
+    modalShow(el.id);
   });
-  /* 
-  var className = document.body.className;
-  if (className == "light-theme") {
-    this.textContent = "Dark";
-  } else {
-    this.textContent = "Light";
-  } */
+
+  newArticle.appendChild(newArticleHeader);
+  newArticle.appendChild(newArticleBody);
+  newArticle.appendChild(newArticleImg);
+  newArticle.appendChild(newArticleButton);
+
+  const articlesContainer = document.getElementById("articles");
+  articlesContainer.appendChild(newArticle);
+}
+
+listItems.forEach((element) => {
+  console.log(element.headline, element.text, element.imgUrl);
+  addElement(element);
+  //element.classList.toggle("test");
+  //element.innerHTML = "<b>test</b>";
 });
